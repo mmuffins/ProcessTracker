b60705574c9af8515e12408b9b2617569bcbb71a
mmuffins <mail@tiv73.com>
2025-01-29 23:25:16 +0000

    add nix deps

---
+++ default.nix
@@ -0,0 +1,37 @@
+{ lib
+, fetchFromGitHub
+, buildDotnetModule
+, dotnetCorePackages
+}:
+
+# dotnet restore --packages out ./ProcessTracker.sln
+# nuget-to-json out > deps.json
+# nix-build -E 'with import <nixpkgs> {}; callPackage ./default.nix {}'
+
+buildDotnetModule rec {
+  pname = "process-tracker";
+  version = "1.0.461";
+
+  meta = with lib; {
+    description = "A cross-platform tool to track and report how long process were running";
+    license = licenses.mit;
+    platforms = platforms.linux;
+  };
+
+  src = ./.;
+
+  # projectFile = [
+  #   "ProcessTrackerService.Core/ProcessTrackerService.Core.csproj"
+  #   "ProcessTrackerService.Infrastructure/ProcessTrackerService.Infrastructure.csproj"
+  #   "ProcessTrackerService/ProcessTrackerService.csproj"
+  # ];
+  projectFile = [
+    "ProcessTrackerService.Core/ProcessTrackerService.Core.csproj"
+    "ProcessTrackerService.Infrastructure/ProcessTrackerService.Infrastructure.csproj"
+  ];
+  nugetDeps = ./deps.json;
+  executables = [ "processtracker" ];
+
+  dotnet-sdk = dotnetCorePackages.sdk_8_0;
+  dotnet-runtime = dotnetCorePackages.runtime_8_0;
+}
