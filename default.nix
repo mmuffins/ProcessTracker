98bac2e824841a07e4d887e3830fc345c466bf05
mmuffins <mail@tiv73.com>
2025-02-03 20:18:23 +0000

    Update file

--- default.nix
+++ default.nix
@@ -20,6 +20,9 @@ buildDotnetModule rec {
     platforms = platforms.linux;
   };
 
+  dotnet-sdk = dotnetCorePackages.sdk_9_0;
+  dotnet-runtime = dotnetCorePackages.runtime_9_0;
+
   src = ./.;
 
   projectFile = [
@@ -29,15 +32,4 @@ buildDotnetModule rec {
   nugetDeps = ./deps.json;
   executables = [ "processtracker" ];
 
-  dotnet-sdk = dotnetCorePackages.sdk_9_0;
-  dotnet-runtime = dotnetCorePackages.runtime_9_0;
-  # buildPhase = ''
-  #   tmp="$(mktemp -d)"
-  #   dotnet publish ProcessTrackerService --configuration Relase --runtime linux-x64 --framework net8.0 --self-contained false -p:PublishSingleFile=true -p:PublishTrimmed=false -p:PublishReadyToRun=false --output publish
-  # '';
-
-  # installPhase = ''
-  #   mkdir -p $out
-  #   cp -r publish $out/bin
-  # '';
 }
